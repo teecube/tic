f32780730a63c76b9d78a83f80f783b8eae1debb
Mathieu Debove <mad@teecu.be>
2017-01-21 18:58:00 +0100

    [ci skip] improving release scenario

--- release.sh
+++ release.sh
@@ -11,3 +11,16 @@ git config --global push.default upstream
 git branch -d $GIT_BRANCH_TO_RELEASE_FROM
 git checkout -b $GIT_BRANCH_TO_RELEASE_FROM remotes/origin/$GIT_BRANCH_TO_RELEASE_FROM
 git branch --set-upstream-to=origin/$GIT_BRANCH_TO_RELEASE_FROM $GIT_BRANCH_TO_RELEASE_FROM
+
+# releasing T3
+git clone https://git.teecu.be/teecube/t3.git
+cd t3
+git checkout master
+RELEASE_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' exec:exec)
+DEV_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-${parsedVersion.qualifier}' exec:exec)
+git checkout release
+sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
+sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
+git add release.properties && git commit -m "Triggering release"
+git push origin release
+
