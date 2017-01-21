6bfa0dd85244da6f060eedaaf4cdd00396201461
Mathieu Debove <mad@teecu.be>
2017-01-21 18:47:15 +0100

    [ci skip] improving release scenario

--- fullRelease.sh
+++ fullRelease.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 t3ReleaseVersion t3DevVersion ticReleaseVersion ticDevVersion" >&2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 ticReleaseVersion ticDevVersion" >&2
     exit 1
 fi
 
 # releasing T3
 git clone https://git.teecu.be/teecube/t3.git
 cd t3
+git checkout master
+RELEASE_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion}' exec:exec)
+DEV_VERSION=$(mvn -q -N build-helper:parse-version -Dexec.executable="echo" -Dexec.args='${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-${parsedVersion.qualifier}' exec:exec)
 git checkout release
-RELEASE_VERSION=$1 && sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
-DEV_VERSION=$2 && sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
+sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
+sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
 git add release.properties && git commit -m "Triggering release"
 git push origin release
 
@@ -18,7 +21,7 @@ cd ..
 rm -rf ./t3/
 
 # releasing
-RELEASE_VERSION=$3 && sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
-DEV_VERSION=$4 && sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
+RELEASE_VERSION=$1 && sed -i "s/\(RELEASE_VERSION=\).*\$/\1${RELEASE_VERSION}/" release.properties
+DEV_VERSION=$2 && sed -i "s/\(DEV_VERSION=\).*\$/\1${DEV_VERSION}/" release.properties
 git add release.properties && git commit -m "Triggering release"
 git push origin release
--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-chmod u-x ./release.sh
+chmod $RELEASE_SH_CHMOD ./release.sh
 
 . release.properties
 
-# configure repository and checkout master instead of current release branch
-git config --global user.name "Mathieu Debove"
-git config --global user.email "mad@teecu.be"
+# configure repository and checkout $GIT_BRANCH_TO_RELEASE_FROM instead of current release branch
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
 git config --global push.default upstream
-git branch -d master
-git checkout -b master remotes/origin/master
-git branch --set-upstream-to=origin/master master
+git branch -d $GIT_BRANCH_TO_RELEASE_FROM
+git checkout -b $GIT_BRANCH_TO_RELEASE_FROM remotes/origin/$GIT_BRANCH_TO_RELEASE_FROM
+git branch --set-upstream-to=origin/$GIT_BRANCH_TO_RELEASE_FROM $GIT_BRANCH_TO_RELEASE_FROM
