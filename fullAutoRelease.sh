20bece20da2ecc389fecbfbf540a0c2dfc944ceb
Mathieu Debove <mad@teecu.be>
2017-01-24 17:41:44 +0100

    [ci skip] improving release scenario

---
+++ fullAutoRelease.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# retrieve Maven auto release script
+wget -q https://raw.githubusercontent.com/debovema/maven-auto-releaser/master/mavenAutoRelease.sh -O /tmp/mavenAutoRelease.sh
+chmod u+x /tmp/mavenAutoRelease.sh
+. /tmp/mavenAutoRelease.sh 
+
+# call updateReleaseVersionsAndTrigger from Maven auto release script
+updateReleaseVersionsAndTrigger https://git.teecu.be/teecube/t3.git
+updateReleaseVersionsAndTrigger https://git.teecu.be/teecube/tic.git
+
