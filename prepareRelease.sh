c31eccd747294d1686a89bd63e0488181fbb6603
Mathieu Debove <mad@teecu.be>
2017-01-26 11:37:31 +0100

    [ci skip] updated auto release scripts

--- prepareRelease.sh
+++ prepareRelease.sh
@@ -4,6 +4,8 @@ chmod $RELEASE_SH_CHMOD ./release.sh
 
 . release.properties
 
+$MAVEN_RELEASE_ADDITIONAL_ARGS="-gs /home/m2/settings.xml -P additional,maven,site,t3-release"
+
 # configure repository and checkout $GIT_BRANCH_TO_RELEASE_FROM instead of current release branch
 git config --global user.name $GIT_USER_NAME
 git config --global user.email $GIT_USER_EMAIL
